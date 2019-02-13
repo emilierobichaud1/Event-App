afd05eaf70f2ddff10764e5eef48c2d5e176763e
Francis Dinh <normandy@firemail.cc>
2019-02-13 15:29:02 -0500

    [#163491328] Finish event creation activity

--- TeamRocketEventApp/app/src/main/java/com/example/teamrocketeventapp/EventIndexActivity.java
+++ TeamRocketEventApp/app/src/main/java/com/example/teamrocketeventapp/EventIndexActivity.java
@@ -1,10 +1,12 @@
 package com.example.teamrocketeventapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 public class EventIndexActivity extends AppCompatActivity {
@@ -18,7 +20,7 @@ public class EventIndexActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    mTextMessage.setText(R.string.home);
                     return true;
                 case R.id.navigation_events:
                     mTextMessage.setText(R.string.events);
@@ -36,9 +38,14 @@ public class EventIndexActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_index);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        mTextMessage = findViewById(R.id.message);
+        BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    public void create(View view) {
+        Intent intent = new Intent(this, EventCreateActivity.class);
+        startActivity(intent);
     }
 
 }
