a0c76b7adcb1b6c597822f706cb1bf78a58a3a19
Francis Dinh <normandy@firemail.cc>
2019-02-12 22:42:51 -0500

    [#163491328] Link to event index from navbar

--- TeamRocketEventApp/app/src/main/java/com/example/teamrocketeventapp/EventActivity.java
+++ TeamRocketEventApp/app/src/main/java/com/example/teamrocketeventapp/EventActivity.java
@@ -1,7 +1,13 @@
 package com.example.teamrocketeventapp;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
 public class EventActivity extends AppCompatActivity {
 
@@ -9,5 +15,19 @@ public class EventActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event);
+
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(menuItem -> {
+            switch (menuItem.getItemId()) {
+                case R.id.navigation_events:
+                    // Switch to event index when "Events" button is pressed
+                    Intent intent = new Intent(EventActivity.this,
+                            EventIndexActivity.class);
+                    EventActivity.this.startActivity(intent);
+                    return true;
+            }
+            return false;
+        });
     }
+
 }
