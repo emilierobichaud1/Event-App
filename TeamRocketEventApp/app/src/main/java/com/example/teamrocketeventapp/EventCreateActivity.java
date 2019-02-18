d1a70794ee13180053dc2d9cf78fdc8a75fc8308
aldrinRamiro <aldrin.ramiro27@gmail.com>
2019-02-18 17:31:14 -0500

    [#164016319] Begin working on createEvent backend

--- TeamRocketEventApp/app/src/main/java/com/example/teamrocketeventapp/EventCreateActivity.java
+++ TeamRocketEventApp/app/src/main/java/com/example/teamrocketeventapp/EventCreateActivity.java
@@ -2,12 +2,76 @@ package com.example.teamrocketeventapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class EventCreateActivity extends AppCompatActivity {
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class EventCreateActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private FirebaseDatabase database;
+    private DatabaseReference myRef;
+
+    private Button submitButton;
+    private EditText eventNameText;
+    private EditText dateText;
+    private EditText timeText;
+    private EditText locationText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_create);
+
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference();
+
+        //Get parts of the layout
+        submitButton = (Button) findViewById(R.id.submitButton);
+        eventNameText = (EditText) findViewById(R.id.eventNameEditText;
+        dateText = (EditText) findViewById(R.id.dateEditText;
+        timeText = (EditText) findViewById(R.id.timeEditText);
+        locationText = (EditText) findViewById(R.id.locationEditText);
+
+        submitButton.setOnClickListener(this);
+    }
+
+    private void createEvent(){
+        //Get info from fields
+        String eventName = eventNameText.getText().toString().trim();
+        String date = dateText.getText().toString().trim();
+        String time = timeText.getText().toString().trim();
+        String location = locationText.getText().toString().trim();
+
+        //Error check user input here
+        if (TextUtils.isEmpty(eventName)) {
+            Toast.makeText(this, "Please enter event name", Toast.LENGTH_SHORT).show();  //Toast is popup msg at bottom
+            return; //Return to stop registration
+        }
+        if (TextUtils.isEmpty(date)) {
+            Toast.makeText(this, "Please enter date", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(time)) {
+            Toast.makeText(this, "Please enter time", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(location)) {
+            Toast.makeText(this, "Please enter location", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == submitButton) {
+            createEvent();
+        }
     }
 }
---
+++ TeamRocketEventApp/app/src/main/java/com/example/teamrocketeventapp/EventProperties.java
@@ -0,0 +1,32 @@
+/**
+ * Class is used to save user info in database
+ * Firebase uses Java object to save data
+ */
+
+package com.example.teamrocketeventapp;
+
+public class EventProperties {
+    public String username;
+    public String email;
+    public String bday;
+    public String address;
+
+    public EventProperties(){
+
+    }
+
+    public EventProperties(String username, String email, String bday, String address) {
+        this.username = username;
+        this.email = email;
+        this.bday = bday;
+        this.address = address;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public String getAddress (){
+        return this.address;
+    }
+}
