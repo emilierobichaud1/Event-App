d231ae3756114ac66f1e006c49e3887105f8dcee
Andrew-Juskiewicz <46761840+Andrew-Juskiewicz@users.noreply.github.com>
2019-02-22 17:16:39 -0500

    [#163491441] implemented searching for events by name

--- TeamRocketEventApp/app/src/main/java/com/example/teamrocketeventapp/EventActivity.java
+++ TeamRocketEventApp/app/src/main/java/com/example/teamrocketeventapp/EventActivity.java
@@ -6,15 +6,56 @@ import android.support.design.internal.BottomNavigationItemView;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class EventActivity extends AppCompatActivity {
+
+    EventProperties event;
+    private String eventId;
+
+    //needed to pull data from the database
+    ValueEventListener valueEventListener = new ValueEventListener() {
+        @Override
+        //method that activates upon query
+        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+            if(dataSnapshot.exists()){
+                for(DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    event = snapshot.getValue(EventProperties.class);
+                    Log.d("AAAAAZZZ", event.getName());
+                    loadData();
+                }
+            }
+        }
+
+        @Override
+        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+        }
+    };
+
+
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event);
+        Bundle b = getIntent().getExtras();
 
         BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(menuItem -> {
@@ -28,6 +69,22 @@ public class EventActivity extends AppCompatActivity {
             }
             return false;
         });
+
+        if(b != null){
+            //get the eventproperties object
+            eventId = (String) b.get("eventid");
+
+            DatabaseReference eventsRef = FirebaseDatabase.getInstance().getReference("events");
+            Query query = FirebaseDatabase.getInstance().getReference("events").orderByChild("id").equalTo(eventId);
+            query.addListenerForSingleValueEvent(valueEventListener);
+        }
+    }
+
+
+    private void loadData(){
+        //sets name textbox
+        TextView usernameTextView = findViewById(R.id.eventName);
+        usernameTextView.setText(event.name);
     }
 
 }
