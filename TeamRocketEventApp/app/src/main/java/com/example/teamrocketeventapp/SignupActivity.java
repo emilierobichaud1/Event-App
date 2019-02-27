121ccb9451e03b335c5e3adf9738e3aaadfba986
Ryanandpsp <r_liu_321@hotmail.com>
2019-02-27 02:09:25 -0500

    #164015753 Updated signup username/password constraints

--- TeamRocketEventApp/app/src/main/java/com/example/teamrocketeventapp/SignupActivity.java
+++ TeamRocketEventApp/app/src/main/java/com/example/teamrocketeventapp/SignupActivity.java
@@ -22,6 +22,8 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.Calendar;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SignupActivity extends AppCompatActivity implements View.OnClickListener {
     private FirebaseAuth mAuth;
@@ -94,6 +96,11 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
         String email = emailText.getText().toString().trim();
         String password = passwordText.getText().toString().trim();
         String passwordConf = passwordConfText.getText().toString().trim();
+        Pattern p = Pattern.compile("[^a-z0-9 ]", Pattern.CASE_INSENSITIVE);
+        Matcher m = p.matcher (username);
+        Matcher m2 = p.matcher(password);
+        boolean b = m.find();
+        boolean b2 = m2.find();
 
         //Error check user input here
         if (TextUtils.isEmpty(username)) {
@@ -106,6 +113,19 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
         }
         if (!password.equals(passwordConf)) {
             Toast.makeText(this, "Passwords do not match", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (password.length() < 6) {
+            Toast.makeText(this, "Passwords needs to be 6 characters or more", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (b) {
+            Toast.makeText(this, "Username has special characters", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (b2) {
+            Toast.makeText(this, "Password has special characters", Toast.LENGTH_SHORT).show();
             return;
         }
 
